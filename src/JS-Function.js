9d675869b34cd044ebf10dbb204b5d5713d01591
LC-daoxin <457121827@qq.com>
2019-11-09 23:21:22 +0800

    change

--- src/JS-Function.js
+++ src/JS-Function.js
@@ -52,4 +52,42 @@ function  f(x, y, z) {
     check(arguments);
     return x + y + z
 }
-console.log(f(6,5));
+console.log(f(6,5,7));
+
+var Person1  = function () {
+    this.name = 'dada';
+}
+var Person2 = function () {
+    this.getname = function () {
+        console.log(this.name);
+    }
+    Person1.call(this);//this指向Person2，结果相当于给Person2加了name属性
+}
+var person = new Person2();
+person.getname();       // dada
+console.log(person)
+
+function ff(y) {
+    return this.x + y
+}
+var o = { x:1 };
+var g = ff.bind(o);
+console.log(g(2));
+
+
+function array(a, n) {
+    return Array.prototype.slice.call(a, n || 0)
+}
+function Left(f /* , ... */) {
+    var args = arguments;
+    return function () {
+        var a = array(arguments);
+        a = a.concat(array(args, 1));
+        console.log(a);
+        return f.apply(this, a);
+    }
+}
+var f1 = function (x, y, z) {
+  return console.log(x * (y - z))
+};
+Left(f1, 2)(3, 4);
